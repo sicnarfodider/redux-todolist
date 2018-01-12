ad0339640b055a268ef60c93bdf944615274c965
Francis Redido <sicnarfodider@hotmail.com>
2018-01-11 17:03:29 -0800

    completed working baseline todo, with redux form using dummy data. creation and deletion of data is working

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import todoReducer from './todo-reducer';
-
+import {reducer as formReducer} from 'redux-form';
 
 export default combineReducers({
-  todo: todoReducer
-})
+  todo: todoReducer,
+  form: formReducer,
+
+});
