b10b7f3dea838e77dbe90a78b707a5fb8af7a168
Francis Redido <sicnarfodider@hotmail.com>
2018-01-15 05:34:09 -0800

    deleting item from the list complete

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,28 +5,36 @@ const BASE_URL = 'http://api.reactprototypes.com';
 const API_KEY = '?key=datboipepe3000';
 
 export function addItem(item){
-  const request = axios.post(BASE_URL+'/todos'+API_KEY, item);
-  return{
-    type: types.ADD_ITEM,
-    payload: request
-  }
+    const request = axios.post(BASE_URL+'/todos'+API_KEY, item);
+    return{
+        type: types.ADD_ITEM,
+        payload: request
+    }
 };
 
 export function getItem(){
     const request = axios.get(BASE_URL+'/todos'+API_KEY);
 
     return{
-      type: types.GET_ITEMS,
-      payload: request
+        type: types.GET_ITEMS,
+        payload: request
     }
 }
 
 export function getSingle(id){
-  const request = axios.get(BASE_URL+'/todos/'+ id +API_KEY);
-  return{
-    type: types.GET_SINGLE_ITEM,
-    payload: request
-  }
+    const request = axios.get(BASE_URL+'/todos/'+ id +API_KEY);
+    return{
+        type: types.GET_SINGLE_ITEM,
+        payload: request
+    }
+}
+
+export function deleteItem(id){
+    const request = axios.delete(BASE_URL+'/todos/'+id+API_KEY)
+    return{
+        type: types.DELETE_ITEM,
+        payload: request
+    }
 }
 
 /*
